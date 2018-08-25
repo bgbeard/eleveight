51f5cab6d115a7070ec516510bf3b5cd5a28566b
Bryan Beard <36644739+bgbeard@users.noreply.github.com>
2018-08-24 19:28:21 -0700

    Add files via upload

---
+++ roleRouting/redux/actions.tsx
@@ -0,0 +1,19 @@
+﻿export const getUserData = (loginResponse) => ({
+    type: "GET_USER_DATA",
+    payload: { loginResponse }
+});
+
+export const setProfileImgSrc = (avatarUrl) => ({
+    type: "SET_PROFILE_IMAGE_SRC",
+    payload: { avatarUrl }
+});
+
+export const logoutUser = () => ({
+    type: "LOGOUT_USER"
+});
+
+export const getNewNotif = (notificationResponse) => (
+    {
+        type: "GET_NEW_NOTIF",
+        payload: { notificationResponse }
+    })
---
+++ roleRouting/redux/reducers.tsx
@@ -0,0 +1,66 @@
+﻿import { combineReducers } from "redux";
+
+const initNotifState = {
+    notifForIssue: 0,
+    notifForIssueForAdmin: 0
+};
+
+const notification = (state = initNotifState, action) => {
+
+    const { payload } = action;
+    switch (action.type) {
+        case "GET_NEW_NOTIF": // actions are just javascript objects
+            //   let notificationResponse = payload.notificationResponse;
+            return {
+                ...this.state,
+                notifForIssue: payload.notificationResponse.notifForIssue,
+                notifForIssueForAdmin: payload.notificationResponse.notifForIssueForAdmin
+
+            };
+        default:
+            return state;
+    }
+}
+
+const initUserState = {
+    id: 0,
+    email: "",
+    firstName: "",
+    lastName: "",
+    profileId: 0,
+    roles: [],
+    roleRoutes: [],
+    avatarUrl: "",
+    orgId: 0
+};
+
+const user = (state = initUserState, action) => {
+    // allows me to type payload instead of action.payload
+    const { payload } = action;
+    switch (action.type) {
+        case "GET_USER_DATA":
+            let loginResponse = payload.loginResponse;
+            return {
+                id: loginResponse.id,
+                email: loginResponse.email,
+                firstName: loginResponse.firstName,
+                lastName: loginResponse.lastName,
+                profileId: loginResponse.profileId,
+                roles: loginResponse.roles,
+                roleRoutes: loginResponse.roleRoutes,
+                avatarUrl: loginResponse.avatarUrl,
+                orgId: loginResponse.orgId
+            };
+
+        case "SET_PROFILE_IMAGE_SRC":
+            return { profileImgSrc: payload.profileImgSrc };
+
+        case "LOGOUT_USER":
+            return { userBaseId: 0, role: "", notifications: 0 };
+
+        default:
+            return state;
+    }
+}
+
+export const reducers = combineReducers({ user, notification });
